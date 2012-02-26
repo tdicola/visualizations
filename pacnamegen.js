d9893df1c92146787213b2acbc5311aac5af7599
Tony DiCola <tony@tonydicola.com>
2012-02-26 01:05:42 -0800

    Add unit test for pacnamegen.

--- pacnamegen.js
+++ pacnamegen.js
@@ -3,17 +3,19 @@
 //
 // Generate a random string of text based on the trigrams of political action
 // committee names.
-var trigrams, startKeys, $, FB, document;
+
+// Referenced global variables
+var $, FB, document, exports;
 
 // Return a random int between min and max, inclusive.
-function randomInt(min, max) {
+function random_int(min, max) {
 	"use strict";
 	return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 // Generate a list of starting keys with at least threshold number of ending
 // words.
-function genStartKeys(trigrams, threshold) {
+function gen_start_keys(trigrams, threshold) {
 	"use strict";
 	var startKeys = [],
 		k;
@@ -29,10 +31,10 @@ function genStartKeys(trigrams, threshold) {
 // Generate a random string of text from trigrams.  Try to create a string of
 // text between minWords and maxWords length, however the result might be
 // smaller.
-function genRandomText(trigrams, minWords, maxWords) {
+function gen_random_text(trigrams, startKeys, minWords, maxWords) {
 	"use strict";
-	var max = randomInt(minWords, maxWords),
-		key = startKeys[randomInt(0, startKeys.length - 1)],
+	var max = random_int(minWords, maxWords),
+		key = startKeys[random_int(0, startKeys.length - 1)],
 		n = 2,
 		text = key,
 		words,
@@ -42,7 +44,7 @@ function genRandomText(trigrams, minWords, maxWords) {
 	// can't be found.
 	while ((n < max) && trigrams.hasOwnProperty(key)) {
 		words = trigrams[key];
-		w = words[randomInt(0, words.length - 1)];
+		w = words[random_int(0, words.length - 1)];
 		text = text + ' ' + w;
 		n += 1;
 		// Build the new key from the 2nd and 3rd words of the trigram
@@ -53,37 +55,44 @@ function genRandomText(trigrams, minWords, maxWords) {
 	return text;
 }
 
-// Run code when the page is loaded and ready
-$(document).ready(function () {
-	"use strict";
+// Check if running in browser
+if ($ !== undefined) {
+	// Build experience when DOM is ready
+	$(document).ready(function () {
+		"use strict";
 
-	// Load the trigram dataset and generate a random PAC name.
-	$.getJSON('pac_trigrams.json', function (data) {
-		trigrams = data;
-		startKeys = genStartKeys(trigrams, 2);
+		// Load the trigram dataset and generate a random PAC name.
+		$.getJSON('pac_trigrams.json', function (data) {
+			var trigrams = data,
+				startKeys = gen_start_keys(trigrams, 2);
 
-		$('#generate')
-			.click(function () {
-				$('#name').text(genRandomText(trigrams, 5, 8));
-			})
-			.removeAttr('disabled')
-			.click();
+			$('#generate')
+				.click(function () {
+					$('#name').text(gen_random_text(trigrams, startKeys, 5, 8));
+				})
+				.removeAttr('disabled')
+				.click();
 
-		$('#share')
-			.click(function () {
-				FB.login(function (response) {
-					FB.ui({
-						method: 'feed',
-						link: 'http://tdicola.github.com/visualizations/pacnamegen.html',
-						picture: 'http://tdicola.github.com/visualizations/pac_names_mix_small.png',
-						name: 'Political Action Committee Name Generator',
-						caption: 'I just generated:',
-						description: $('#name').text(),
-						redirect_uri: 'http://tdicola.github.com/visualizations/pacnamegen.html'
-					}, function (response) {});
-				}, { scope: 'publish_stream'});
-			})
-			.removeAttr('disabled');
+			$('#share')
+				.click(function () {
+					FB.login(function (response) {
+						FB.ui({
+							method: 'feed',
+							link: 'http://tdicola.github.com/visualizations/pacnamegen.html',
+							picture: 'http://tdicola.github.com/visualizations/pac_names_mix_small.png',
+							name: 'Political Action Committee Name Generator',
+							caption: 'I just generated:',
+							description: $('#name').text(),
+							redirect_uri: 'http://tdicola.github.com/visualizations/pacnamegen.html'
+						}, function (response) {});
+					}, { scope: 'publish_stream'});
+				})
+				.removeAttr('disabled');
+		});
 	});
-});
-
+} else {
+	// Export functions for module
+	exports.gen_start_keys = gen_start_keys;
+	exports.gen_random_text = gen_random_text;
+	exports.random_int = random_int;
+}
---
+++ test/test_pacnamegen.js
@@ -0,0 +1,45 @@
+var assert = require('assert'),
+	pacnamegen = require('../pacnamegen.js');
+
+suite('PAC Name Gen', function () {
+	var trigrams = { "foo bar": ["baz"],
+					 "bar baz": ["shaz"],
+					 "blah blah": ["blah", "meh"],
+					 "test test2": ["one", "two", "three"],
+					 "test2 test3": ["foo", "bar", "baz", "shaz"] };
+
+	test('random_int returns value between min and max inclusive', function () {
+		var min = 5,
+			max = 10,
+			i = pacnamegen.random_int(min, max);
+		assert.ok(i <= max);
+		assert.ok(i >= min);
+	});
+
+	test('gen_start_keys returns keys with more endings than threshold', function () {
+		var startkeys = pacnamegen.gen_start_keys(trigrams, 2);
+		assert.equal(startkeys.length, 2);
+	});
+
+	test('gen_start_keys returns all keys when threshold is 0', function () {
+		var	startkeys = pacnamegen.gen_start_keys(trigrams, 0);
+		assert.equal(startkeys.length, 5);
+	});
+
+	test('gen_start_keys returns no keys when threshold is too high', function () {
+		var	startkeys = pacnamegen.gen_start_keys(trigrams, 5);
+		assert.equal(startkeys.length, 0);
+	});
+
+	test('gen_random_text generates at least minWords when possible', function () {
+		var startkeys = ["foo bar"],
+			text = pacnamegen.gen_random_text(trigrams, startkeys, 4, 5);
+		assert.equal(text, "foo bar baz shaz");
+	});
+
+	test('gen_random_text generates at most maxWords', function () {
+		var startkeys = ["foo bar"],
+			text = pacnamegen.gen_random_text(trigrams, startkeys, 3, 3);
+		assert.equal(text, "foo bar baz");
+	});
+});
